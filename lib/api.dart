8b0958093a55748495daaf9bca3a019084da8cd6
siddhishah109 <shahsiddhi777@gmail.com>
2023-03-05 11:19:52 +0530

    changes

--- lib/api.dart
+++ lib/api.dart
@@ -4,15 +4,17 @@ import 'package:ecommerce/models/productsm.dart';
 
 import 'package:http/http.dart' as http;
 
-class Api {
-   Future<void> allproducts() async {
-    final response =
-        await http.get(Uri.parse('https://api.escuelajs.co/api/v1/products'));
-    var data = jsonDecode(response.body);
-    //List templist = [];
-    //for (var v in data) {
-     // templist.add(v);
-    //}
-    //return ProductM.productsFromJson(templist);
+allproducts() async {
+  final response =
+      await http.get(Uri.parse('https://api.escuelajs.co/api/v1/products'));
+  var data = ProductM.fromJson(response.body);
+  if (response.statusCode == 200) {
+    print(data);
   }
+
+  //List templist = [];
+  //for (var v in data) {
+  // templist.add(v);
+  //}
+  //return ProductM.productsFromJson(templist);
 }
